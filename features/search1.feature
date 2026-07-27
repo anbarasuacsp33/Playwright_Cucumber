d63f859719671ffc750bf76ecb810d99043ec2f2
anbarasuacsp33 <anbarasuacsp@gmail.com>
2026-07-27 08:01:25 +0530

    Todayscommit_ScenarioOutlineImplementation_07272026

--- features/search1.feature
+++ features/search1.feature
@@ -4,12 +4,27 @@ Feature: Search at second time
     Scenario: To Verify Possitive search
         Given User in ebay home page
         When Enter product name in a search field
-        And Click Search buttons
+        And Click Search buttons new
         Then I verified search results
 
     @ScenrioTagsearch2
     Scenario: To Verify Negative search
         Given User in ebay home page
         When Enter invalid product name in search field
-        When Click Search button
+        And Click Search buttons new
         Then I verified no search results
+
+    @ScenrioTagsearch3
+    Scenario Outline: Scenario Outline name
+        Given User in ebay home page
+        When Search For A Product "<Product>"
+        And Click Search buttons new
+        And Verify the search result
+        And Click the first product from the search result
+        Then Verify product detail page
+        Examples:
+            | Product |
+            | Iphone  |
+            | samsung |
+            | Moto    |
+            | sgfsgff |
--- features/stepdef/search1step.js
+++ features/stepdef/search1step.js
@@ -11,8 +11,9 @@ await this.page.locator("#gh-ac").fill("Laptop");
 });
        
 
-When('Click Search buttons', async function () {
+When('Click Search buttons new', async function () {
 await this.page.locator("#gh-search-btn").click();
+// await this.page.waitForTimeout(5000);
 
 });
        
@@ -31,13 +32,38 @@ When('Enter invalid product name in search field', async function () {
     await this.page.locator("#gh-ac").fill("abc-test");
 });
 
-When('Click Search button', async function () {
-    await this.page.locator("#gh-search-btn").click();
-});
 
 Then('I verified no search results', async function () {
 
     await expect(this.page.locator(".srp-controls__count-heading")).toContainText("0 results for abc-test");
 
     await expect(this.page.locator("h1")).toContainText("No exact matches found");
+});
+
+
+When('Search For A Product {string}', async function (Product) {
+    await this.page.locator("#gh-ac").fill(Product);
+});
+
+
+
+
+When('Verify the search result', async function () {
+
+  
+
+});
+
+
+When('Click the first product from the search result', async function () {
+
+
+
+});
+
+
+Then('Verify product detail page', async function () {
+
+
+
 });
