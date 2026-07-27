08a485df5f1c4fd9df7f57208027e811cdc11151
PranuK1308 <Pranitkhadse0803@gmail.com>
2026-07-27 07:22:11 +0530

    Pranita_Changes

--- features/search1.feature
+++ features/search1.feature
@@ -1,4 +1,4 @@
-@GlobalTagSearch1
+@GlobalTagSearch1   @smoke
 Feature: Search at second time
     @ScenrioTagsearch1
     Scenario: To Verify Possitive search
@@ -6,3 +6,10 @@ Feature: Search at second time
         When Enter product name in a search field
         And Click Search buttons
         Then I verified search results
+
+    @ScenrioTagsearch2
+    Scenario: To Verify Negative search
+        Given User in ebay home page
+        When Enter invalid product name in search field
+        When Click Search button
+        Then I verified no search results
--- features/stepdef/search1step.js
+++ features/stepdef/search1step.js
@@ -1,5 +1,5 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
-
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect } from '@playwright/test';
 
 Given('User in ebay home page', async function () {
     await this.page.goto("https://www.ebay.com")
@@ -7,15 +7,37 @@ Given('User in ebay home page', async function () {
 
 
 When('Enter product name in a search field', async function () {
-
+await this.page.locator("#gh-ac").fill("Laptop");
 });
        
 
 When('Click Search buttons', async function () {
+await this.page.locator("#gh-search-btn").click();
 
 });
        
 
 Then('I verified search results', async function () {
+  await expect(this.page).toHaveURL('https://www.ebay.com/sch/i.html?_nkw=Laptop&_sacat=0&_from=R40&_trksid=p4624852.m570.l1313'
+  );
 
+  const heading = this.page.locator('.srp-controls__count-heading');
+
+  await expect(heading).toContainText('Laptop');
+ });
+
+
+When('Enter invalid product name in search field', async function () {
+    await this.page.locator("#gh-ac").fill("abc-test");
+});
+
+When('Click Search button', async function () {
+    await this.page.locator("#gh-search-btn").click();
+});
+
+Then('I verified no search results', async function () {
+
+    await expect(this.page.locator(".srp-controls__count-heading")).toContainText("0 results for abc-test");
+
+    await expect(this.page.locator("h1")).toContainText("No exact matches found");
 });
